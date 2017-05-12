6e3526292911bf56fd7a2777e5cbeda3e2020b97
anankran <nankran@inventati.org>
2017-05-12 16:07:11 -0300

    correcting ajax

--- public/ajaxResponse.php
+++ public/ajaxResponse.php
@@ -1,16 +1,18 @@
 <?php
 if(isset($url[1])):
 	if(isset($url[2])):
-		//try{
+		try{
+			var_dump($url);
+			die;
 			$controllerName = 'Controller\\'.ucfirst($url[1]).'Controller';
 			$controller = new $controllerName();
 			$field1 = isset($url[3])?$url[3]:null;
 			$field2 = isset($url[4])?$url[4]:null;
 			print $controller->$url[2]($field1,$field2);
-		// }	catch(Exception $error) {
-		// 	header('HTTP/1.0 404 Not found');
-		//   exit($error->getMessage());
-		// }
+		}	catch(Exception $error) {
+			header('HTTP/1.0 404 Not found');
+		  exit($error->getMessage());
+		}
 		exit;
 	else:
 		header('HTTP/1.0 403 Forbidden');
