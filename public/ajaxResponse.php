9e872c328b99eed54a0c25bf1c25f1a1500d2c6d
anankran <nankran@inventati.org>
2017-05-12 16:36:02 -0300

    correcting ajax

--- public/ajaxResponse.php
+++ public/ajaxResponse.php
@@ -1,24 +1,19 @@
 <?php
-if(isset($url[1])):
-	if(isset($url[2])):
-		$controllerName = 'Controller\\'.ucfirst($url[1]).'Controller';
-		$controller = new $controllerName();
-		$field1 = isset($url[3])?$url[3]:null;
-		$field2 = isset($url[4])?$url[4]:null;
-		$method = (string)$url[2];
-		if(method_exists($controller, $method) && is_callable(array($controller, $method))):
-			print $method;
-			//print $controller->$method($field1,$field2);
-		else:
-			header('HTTP/1.0 404 Not Found');
-	    print '<h1>Not Found!</h1>';
-		endif;
-		exit;
+$controllerName = 'Controller\\'.ucfirst($url[1]).'Controller';
+if(class_exists($controllerName)):
+	$controller = new $controllerName();
+	$field1 = isset($url[3])?$url[3]:null;
+	$field2 = isset($url[4])?$url[4]:null;
+	$method = (string)$url[2];
+	if(method_exists($controller, $method) && is_callable(array($controller, $method))):
+		print $method;
+		//print $controller->$method($field1,$field2);
 	else:
-		header('HTTP/1.0 403 Forbidden');
-    exit('<h1>Not Found!</h1>');
+		header('HTTP/1.0 404 Not Found');
+    print '<h1>Not Found!</h1>';
 	endif;
 else:
-	header('HTTP/1.0 403 Forbidden');
-	exit('<h1>Not Found!</h1>');
+	header('HTTP/1.0 404 Not Found');
+	print '<h1>Not Found!</h1>';
 endif;
+exit;
